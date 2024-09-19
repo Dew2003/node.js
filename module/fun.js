9e6c34e193eb4598d3a74e2f8c52cc1a1df6c91c
Dewashish Dwivedi <152517848+Dew2003@users.noreply.github.com>
2024-09-19 09:10:33 +0000

    funadd

---
+++ module/fun.js
@@ -0,0 +1,26 @@
+/*function addNums(a,b)
+{
+    return a+b;
+}
+
+function multiplyNums(a,b)
+{
+    return a*b;
+}
+
+console.log("add:",addNums(10,20));
+console.log("multiply:",multiplyNums(10,20)); */
+let addNums = (a,b) =>
+    {
+        return a+b ;
+    }
+    
+   let mul = (a,b) => 
+    {
+        return a*b;
+    }
+
+
+
+
+    export {addNums,mul};
---
+++ us.js
@@ -0,0 +1,4 @@
+import { addNums,mul } from "./module/funct.js";
+
+console.log("add: ",addNums(10,20));
+console.log("add: ",mul(10,20));
